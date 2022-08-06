718338a4a30cc8339f391dbd65d302102a611c36
VoltChange <83451689+VoltChange@users.noreply.github.com>
2022-08-06 15:38:32 +0800

    教师添加课程原型

--- DB_BACKEND/Back/Controllers/CourseController.cs
+++ DB_BACKEND/Back/Controllers/CourseController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Back.Entity;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -33,10 +34,56 @@ namespace Back.Controllers
             return "value";
         }
 
-        // POST api/<CourseController>
+        // 教师添加课程
         [HttpPost]
-        public void Post([FromBody] string value)
+        public string PostCourse()
         {
+
+            Message message = new Message();
+            message.errorCode = 300;
+            StringValues token = default(StringValues);
+            if (Request.Headers.TryGetValue("token", out token))//验证token教师身份
+            {
+
+                var data = Token.VerifyToken(token);
+                if (data != null)
+                {
+                    decimal id = (decimal)data["id"];
+                    var iUser = from i in _Context.Users
+                                where i.UserId == id && i.UserType == 1
+                                select i;
+                    User ins = iUser.FirstOrDefault();
+                    if (ins != null)
+                    {
+                        //验证教师身份成功
+                        //添加课程信息
+                        string coursename = Request.Form["coursename"];
+                        string timeslot = Request.Form["timeslot"];
+                        byte credit = byte.Parse(Request.Form["credit"]);
+                        bool isrequired = bool.Parse(Request.Form["isrequired"]);
+                        string year = Request.Form["year"];
+                        string semester = Request.Form["semester"];
+                        Course course = new Course();
+                        course.CourseName = coursename;
+                        course.TimeSlot = timeslot;
+                        course.Credit = credit;
+                        course.IsRequired = isrequired;
+                        course.Year = year;
+                        course.Semester = semester;
+                        _Context.Courses.Add(course);
+                        _Context.SaveChanges();
+                        message.errorCode = 200;
+                        return message.ReturnJson();
+                    }
+                    else
+                    {
+                        message.errorCode = 201;
+                        return message.ReturnJson();
+
+                    }
+                }
+            }
+            return message.ReturnJson();
         }
 
         // PUT api/<CourseController>/5
@@ -50,5 +97,6 @@ namespace Back.Controllers
         public void Delete(int id)
         {
         }
+
     }
 }
