68fe05a94be03a1875f96da8481bd971dde238a3
VoltChange <83451689+VoltChange@users.noreply.github.com>
2022-08-09 15:15:25 +0800

    教务创建考试

--- DB_BACKEND/Back/Controllers/ExamController.cs
+++ DB_BACKEND/Back/Controllers/ExamController.cs
@@ -27,5 +27,62 @@ namespace Back.Controllers
             message.data["ExamsList"] = _Context.Exams.ToList();
             return message.ReturnJson();
         }
+
+        // 教务创建添加考试
+        [HttpPost("create")]
+        public string PostExam()
+        {
+
+            Message message = new Message();
+            message.errorCode = 300;
+            StringValues token = default(StringValues);
+            if (Request.Headers.TryGetValue("token", out token))//验证token教务身份
+            {
+
+                var data = Token.VerifyToken(token);
+                if (data != null)
+                {
+                    decimal id = (decimal)data["id"];
+                    var aUser = from a in _Context.Users
+                                where a.UserId == id && a.UserType == 2
+                                select a;
+                    User admin = aUser.FirstOrDefault();
+                    if (admin != null)
+                    {
+                        //验证教务身份成功
+                        //添加考试信息
+                        decimal courseid= Decimal.Parse(Request.Form["courseid"]);
+                        int meetingid = int.Parse(Request.Form["meetingid"]);
+                        // 格式：2022-08-08T21:00:00
+                        DateTime starttime = DateTime.Parse(Request.Form["starttime"]);
+                        DateTime endtime = DateTime.Parse(Request.Form["endtime"]);
+                        Exam exam = new Exam();
+                        exam.CourseId = courseid;
+                        exam.StartTime = starttime;
+                        exam.EndTime = endtime;
+                        exam.MeetingId = meetingid;
+                        try
+                        {
+                            _Context.Exams.Add(exam);
+                            _Context.SaveChanges();
+                        }
+                        catch
+                        {
+                            message.errorCode = 202;//数据库更新失败
+                            return message.ReturnJson();
+                        }
+                        message.errorCode = 200;
+                        return message.ReturnJson();
+                    }
+                    else
+                    {
+                        message.errorCode = 201;//身份验证失败
+                        return message.ReturnJson();
+
+                    }
+                }
+            }
+            return message.ReturnJson();
+        }
     }
 }
